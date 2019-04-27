bb000e7c9f9e8df13d8bde1715ea33526ee2d86d
Trí <asylumcrow.code@gmail.com>
2019-04-28 03:03:16 +0700

    Weak Warrior Animation and Input

---
+++ Weak Warrior/Assets/Scripts/Game/Player/PlayerInputListener.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputListener : MonoSingleton<PlayerInputListener>
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		InputHandle();
+    }
+
+    public void Attack(bool direction) //true for left, false for right
+    {
+		PlayerStateController.Instance.Attack(direction);
+    }
+
+	public void Skill()
+	{
+        PlayerStateController.Instance.Skill();
+	}
+
+	public void InputHandle() 
+	{
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Attack(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Attack(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+			Skill();
+        }
+	}
+}
